f5cfa5dcf283e09e83023a2b3fa34768601d3a6e
Quy Nguyen <ntkquy238@gmail.com>
2025-04-18 17:44:16 +0700

    Change picture and Complete Website

--- seeders/20250403005011-comment.js
+++ seeders/20250403005011-comment.js
@@ -8,7 +8,7 @@ module.exports = {
         id: 1,
         postId: 1, 
         title: 'Great Article',
-        content: '<p>Donec id erum quidem rerumd facilis est et expedita distinctio lorem ipsum dolorlit non mi portas sats eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna..</p>',
+        content: '<p>Absolutely love the peaceful vibe in this photo. Makes me want to pack my bags and go explore right now!</p>',
         date: '2024-03-04', 
         createdAt: new Date(),
         updatedAt: new Date() 
@@ -16,8 +16,8 @@ module.exports = {
       { 
         id: 2,
         postId: 1, 
-        title: 'A quick question',
-        content: '<p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p <p>Donec id erum quidem rerumd facilis est et expedita distinctio lorem ipsum dolorlit non mi portas sats eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna.</p>',
+        title: 'Such a beautiful journeys!',
+        content: '<p>Your blog always takes me on the most beautiful journeys. I love how you capture the soul of every destination.</p>',
         date: '2023-05-12', 
         createdAt: new Date(),
         updatedAt: new Date() 
@@ -26,7 +26,7 @@ module.exports = {
         id: 3, 
         postId: 1, 
         title: 'Thank you!',
-        content: '<p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>',
+        content: '<p>This view is breathtaking — it feels like a hidden gem waiting to be discovered. Thanks for sharing such a magical spot with us!</p>',
         date: '2013-05-10', 
         createdAt: new Date(),
         updatedAt: new Date() 
